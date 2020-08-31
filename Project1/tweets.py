985f9cdeb07794f862dd3e60935118201500a839
Michael Telahun <m0tela01@louisville.edu>
2020-08-30 21:20:06 -0400

    Fix small issue

--- Project1/tweets.py
+++ Project1/tweets.py
@@ -3,10 +3,7 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 
 #Variables that contains the user credentials to access Twitter API 
-access_token = "762075386-hddiJG8pThN9sszp46hJxpmSkdREtADrTfCmvSuU" #"ENTER YOUR ACCESS TOKEN"
-access_token_secret = "KaqjJiYG1IlppwmGYwUjf7eDR6eoJKD6Dw7AKsmSnnJjc" #"ENTER YOUR ACCESS TOKEN SECRET"
-consumer_key = "Dw8ZJRGZ0sdjm530Tg60NUWvq" #"ENTER YOUR API KEY"
-consumer_secret = "LQ8qu88jWG3LgIbpL4gcBWJZf8FLi12cAmK9HUcVYX1ZZ0KrXX" #"ENTER YOUR API SECRET"
+
 
 
 #This is a basic listener that just prints received tweets to stdout.
