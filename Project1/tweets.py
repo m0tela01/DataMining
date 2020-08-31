abc488786967c82acbb85fbe86cb74506958b872
Michael Telahun <m0tela01@louisville.edu>
2020-08-30 21:13:06 -0400

    Add initial project 1 things

--- Project1/tweets.py
+++ Project1/tweets.py
@@ -3,10 +3,10 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 
 #Variables that contains the user credentials to access Twitter API 
-access_token = "ENTER YOUR ACCESS TOKEN"
-access_token_secret = "ENTER YOUR ACCESS TOKEN SECRET"
-consumer_key = "ENTER YOUR API KEY"
-consumer_secret = "ENTER YOUR API SECRET"
+access_token = "762075386-hddiJG8pThN9sszp46hJxpmSkdREtADrTfCmvSuU" #"ENTER YOUR ACCESS TOKEN"
+access_token_secret = "KaqjJiYG1IlppwmGYwUjf7eDR6eoJKD6Dw7AKsmSnnJjc" #"ENTER YOUR ACCESS TOKEN SECRET"
+consumer_key = "Dw8ZJRGZ0sdjm530Tg60NUWvq" #"ENTER YOUR API KEY"
+consumer_secret = "LQ8qu88jWG3LgIbpL4gcBWJZf8FLi12cAmK9HUcVYX1ZZ0KrXX" #"ENTER YOUR API SECRET"
 
 
 #This is a basic listener that just prints received tweets to stdout.
@@ -29,4 +29,4 @@ if __name__ == '__main__':
     stream = Stream(auth, l)
 
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['football', 'soccer', 'FIFA'])
+    stream.filter(track=['MOBA', 'MMORPG', 'MMO', 'World of Warcraft', 'DOTA2', 'League of Legends'])
