0b8ac8dff8babfc67bee8909cd70ef2e25d19cf3
Michael Telahun <m0tela01@louisville.edu>
2020-09-07 18:53:37 -0400

    Add some plots of data

--- Project1/tweets.py
+++ Project1/tweets.py
@@ -3,18 +3,21 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 
 #Variables that contains the user credentials to access Twitter API 
-
+access_token = "762075386-hddiJG8pThN9sszp46hJxpmSkdREtADrTfCmvSuU" #"ENTER YOUR ACCESS TOKEN"
+access_token_secret = "KaqjJiYG1IlppwmGYwUjf7eDR6eoJKD6Dw7AKsmSnnJjc" #"ENTER YOUR ACCESS TOKEN SECRET"
+consumer_key = "Dw8ZJRGZ0sdjm530Tg60NUWvq" #"ENTER YOUR API KEY"
+consumer_secret = "LQ8qu88jWG3LgIbpL4gcBWJZf8FLi12cAmK9HUcVYX1ZZ0KrXX" #"ENTER YOUR API SECRET"
 
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
-        print data
+        print(data)
         return True
 
     def on_error(self, status):
-        print status
+        print(status)
 
 
 if __name__ == '__main__':
@@ -26,4 +29,6 @@ if __name__ == '__main__':
     stream = Stream(auth, l)
 
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['MOBA', 'MMORPG', 'MMO', 'World of Warcraft', 'DOTA2', 'League of Legends'])
+    stream.filter(track=['Online Games', 'MOBA', 'MMORPG', 'MMO',\
+        'World of Warcraft', 'Elder Scrolls Online', 'Guild Wars 2', 'Final Fantasy 14', 'Black Desert Online', 'RuneScape',\
+        'Dota 2', 'League of Legends', 'Area of Valor', 'Heros of the Strom'])
